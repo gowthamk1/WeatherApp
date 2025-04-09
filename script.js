f43444bdbcda63cc7247bf8b323f7ae736ef0dc2
gowthamkpvt@gmail.com <gowthamkpvt@gmail.com>
2025-04-09 15:31:15 +0530

    updated script file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const weatherIcon = document.querySelector(".weather-icon");
+const weatherIcon = document.querySelector(".weather-icons");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -17,27 +17,25 @@ async function checkWeather(city){
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
     if(data.weather[0].main == "Clouds"){
-        document.querySelector(".weather-icon").src = "images/clouds.png";
+        weatherIcon.src = "images/clouds.png";
     }
     else if(data.weather[0].main == "Clear"){
-        document.querySelector(".weather-icon").src = "images/clear.png";
+        weatherIcon.src = "images/clear.png";
     }
     else if(data.weather[0].main == "Rain"){
-        document.querySelector(".weather-icon").src = "images/rain.png";
+        weatherIcon.src = "images/rain.png";
     }
     else if(data.weather[0].main == "Drizzle"){
-        document.querySelector(".weather-icon").src = "images/drizzle.png";
+        weatherIcon.src = "images/drizzle.png";
     }
     else if(data.weather[0].main == "Mist"){
-        document.querySelector(".weather-icon").src = "images/mist.png";
+        weatherIcon.src = "images/mist.png";
     }
     else if(data.weather[0].main == "Snow"){
-        document.querySelector(".weather-icon").src = "images/snow.png";
+        weatherIcon.src = "images/snow.png";
     }
 }
 
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
 })
-
-checkWeather()
